bab80db3385d202dcc35cd79c9f87ee23aea754b
rohith <youremailaddress@_Example_ .com>
2024-02-13 09:08:53 +0530

    1

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import Navigation from './Components/Navigation'
 import CourseConatiner from './Components/CourseContainer'
 import FilterSection from './Components/FilterSection'
 function App() {
-  return <div className='main-frame'>
-    <Navigation />
-    <div className='main-frame-2'>
-    <ProfileContainer />
-    <FilterSection/>
-    <CourseConatiner/>
-    </div>
+  return <div></div>; 
+
+  //Write Your Own code in the div
+
+  
+  // <div className='main-frame'>
+  //   <Navigation />
+  //   <div className='main-frame-2'>
+  //   <ProfileContainer />
+  //   <FilterSection/>
+  //   <CourseConatiner/>
+  //   </div>
     
-  </div>;
+  // </div>;
+  
 }
 
 export default App;
