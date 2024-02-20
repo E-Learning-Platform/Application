9cf6b0869494f3b8a5ddf433795786611b2f2858
rohith <youremailaddress@_Example_ .com>
2024-02-20 09:49:16 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
-// import ProfileContainer from './MainPage/Components/ProfileContainer'
-// import Navigation from './MainPage/Components/Navigation'
-// import CourseConatiner from './MainPage/Components/CourseContainer'
-// import FilterSection from './MainPage/Components/FilterSection'
+import ProfileContainer from './MainPage/Components/ProfileContainer'
+import Navigation from './MainPage/Components/Navigation'
+import CourseConatiner from './MainPage/Components/CourseContainer'
+import FilterSection from './MainPage/Components/FilterSection'
 function App() {
-  return <div>
-    <>Hello World</>
-  </div>; 
+  return 
+  // <div>
+  //   <>Hello World</>
+  // </div>; 
 
   //Write Your Own code in the div
+  
 
   
   // <div className='main-frame'>
